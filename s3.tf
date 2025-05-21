bcc37511fc7b7e3312803a4318b18b22cc5c0a08
OwenP04 <opalukaitis@gmail.com>
2025-05-21 11:44:40 -0400

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,37 +1,50 @@
-resource "aws_s3_bucket" "plunder_assets" {
+resource "aws_s3_bucket" "web" {
   bucket = "plunder-cove-assets-bucket"
   force_destroy = true
 }
 
-resource "aws_s3_bucket_ownership_controls" "plunder_assets" {
-  bucket = aws_s3_bucket.plunder_assets.id
+resource "aws_s3_bucket_ownership_controls" "web" {
+  bucket = aws_s3_bucket.web.id
 
   rule {
     object_ownership = "BucketOwnerEnforced"
   }
 }
 
-resource "aws_s3_bucket_public_access_block" "plunder_assets" {
-  bucket = aws_s3_bucket.plunder_assets.id
+resource "aws_s3_bucket_public_access_block" "web" {
+  bucket = aws_s3_bucket.web.id
 
   block_public_acls       = true
-  block_public_policy     = false
   ignore_public_acls      = true
+  block_public_policy     = false
   restrict_public_buckets = false
 }
 
-resource "aws_s3_bucket_policy" "public_read_policy" {
-  bucket = aws_s3_bucket.plunder_assets.id
+resource "aws_s3_bucket_policy" "web_public" {
+  bucket = aws_s3_bucket.web.id
 
   policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
       {
-        Effect = "Allow"
+        Sid       = "PublicReadGetObject"
+        Effect    = "Allow"
         Principal = "*"
-        Action = ["s3:GetObject"]
-        Resource = "${aws_s3_bucket.plunder_assets.arn}/*"
+        Action    = "s3:GetObject"
+        Resource  = "${aws_s3_bucket.web.arn}/*"
       }
     ]
   })
+}
+
+resource "aws_s3_bucket_website_configuration" "web" {
+  bucket = aws_s3_bucket.web.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "index.html"
+  }
 }
