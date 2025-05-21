a870ef0ef3e4812d8b172770a065fe0c7c36aaf3
OwenP04 <opalukaitis@gmail.com>
2025-05-21 11:18:57 -0400

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,26 +1,37 @@
-resource "aws_s3_bucket" "plunder_bucket" {
+resource "aws_s3_bucket" "plunder_assets" {
   bucket = "plunder-cove-assets-bucket"
+  force_destroy = true
+}
 
-  tags = {
-    Name        = "PlunderCoveAssets"
-    Environment = "Production"
-    Project     = "PlunderCove"
+resource "aws_s3_bucket_ownership_controls" "plunder_assets" {
+  bucket = aws_s3_bucket.plunder_assets.id
+
+  rule {
+    object_ownership = "BucketOwnerEnforced"
   }
 }
 
-resource "aws_s3_bucket_versioning" "plunder_bucket_versioning" {
-  bucket = aws_s3_bucket.plunder_bucket.id
-
-  versioning_configuration {
-    status = "Enabled"
-  }
-}
-
-resource "aws_s3_bucket_public_access_block" "plunder_bucket_block" {
-  bucket = aws_s3_bucket.plunder_bucket.id
+resource "aws_s3_bucket_public_access_block" "plunder_assets" {
+  bucket = aws_s3_bucket.plunder_assets.id
 
   block_public_acls       = true
-  block_public_policy     = true
+  block_public_policy     = false
   ignore_public_acls      = true
-  restrict_public_buckets = true
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_policy" "public_read_policy" {
+  bucket = aws_s3_bucket.plunder_assets.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = "*"
+        Action = ["s3:GetObject"]
+        Resource = "${aws_s3_bucket.plunder_assets.arn}/*"
+      }
+    ]
+  })
 }
