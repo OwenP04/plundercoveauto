12a1e91b747bfb8c190a175c116e7e0b89d7dffc
OwenP04 <opalukaitis@gmail.com>
2025-05-23 12:20:35 -0400

    Update security_group.tf

--- security_group.tf
+++ security_group.tf
@@ -31,3 +31,27 @@ resource "aws_security_group" "plunder_sg" {
     Name = "PlunderCoveSG"
   }
 }
+resource "aws_security_group" "eb_sg" {
+  name        = "plunder-eb-sg"
+  description = "Security group for Plunder Cove ECS and ALB"
+  vpc_id      = aws_vpc.plunder_vpc.id # Ensure plunder_vpc is defined in main.tf
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Project     = "PlunderCove"
+    Environment = "Production"
+  }
+}
